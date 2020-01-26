ef6e732cbf92d55d83571ef3f3296ded92e8ccc5
qualpan1 <daniil.orekhov15@gmail.com>
2020-01-25 19:51:27 -0500

    regexes for prices

---
+++ price_string_matching.py
@@ -0,0 +1,28 @@
+#   \$([0-9]+)\/(lb|bag)    prices of type $173/lb
+#   ([0-9]+)\¢\/(lb|bag)    prices of type 13¢/lb
+
+#   ([0-9]+)\/\($|¢)([0-9]+)    prices of type 4/$3
+#   ([0-9]+)\/([0-9]+)\¢   prices of type 4/3¢
+
+#   FREE    for "buy one get one free"
+#   HALF OFF!
+#   ([0-9]+)\% OFF   for type 12% OFF
+
+#   \$([0-9]+)          for type $37
+#   ([0-9]+)\¢          for type 37¢
+
+#   \$([0-9]+) OFF
+#   ([0-9]+)\¢ OFF
+
+#   \$([0-9]+) OFF PER (lb|bag)
+#   ([0-9]+)\¢ OFF PER (lb|bag)
+
+
+
+
+
+
+
+
+
+
