5473cf5810af19529ccd9b09161d1eff05afa390
qualpan1 <daniil.orekhov15@gmail.com>
2020-01-25 21:00:28 -0500

    better regex

--- price_string_matching.py
+++ price_string_matching.py
@@ -1,28 +1,29 @@
-#   \$([0-9]+)\/(lb|bag)    prices of type $173/lb
-#   ([0-9]+)\¢\/(lb|bag)    prices of type 13¢/lb
 
-#   ([0-9]+)\/\($|¢)([0-9]+)    prices of type 4/$3
-#   ([0-9]+)\/([0-9]+)\¢   prices of type 4/3¢
-
-#   FREE    for "buy one get one free"
-#   HALF OFF!
-#   ([0-9]+)\% OFF   for type 12% OFF
-
-#   \$([0-9]+)          for type $37
-#   ([0-9]+)\¢          for type 37¢
-
-#   \$([0-9]+) OFF
-#   ([0-9]+)\¢ OFF
-
-#   \$([0-9]+) OFF PER POUND
-#   ([0-9]+)\¢ OFF PER POUND
+import re
+import pandas as pd
 
 
+def get_units():
+    df_units = pd.read_csv("files/units_dictionary.csv", header=0)
+    units = df_units['units'].values
+    return units
 
 
+DOLLAR_PRICE_PER_UNIT = re.compile(r'\$([0-9]+)/(' + '|'.join(get_units()) + r')')
+CENT_PRICE_PER_UNIT = re.compile(r'([0-9]+)¢/(' + '|'.join(get_units()) + r')')
 
+UNITS_PER_DOLLAR_PRICE = re.compile(r'([0-9]+)/\($|¢)([0-9]+)')
+UNITS_PER_CENT_PRICE = re.compile(r'([0-9]+)/([0-9]+)¢')
 
+BUY_ONE_GET_ONE_FREE = re.compile(r'FREE')
+HALF_OFF = re.compile(r'HALF\sOFF!')
+PERCENTAGE_OFF = re.compile(r'([0-9]+)%\sOFF')
 
+DOLLAR_PRICE = re.compile(r'\$([0-9]+)')
+CENT_PRICE = re.compile(r'([0-9]+)¢')
 
+DOLLAR_OFF = re.compile(r'\$([0-9]+)\sOFF')
+CENT_OFF = re.compile(r'([0-9]+)¢\sOFF')
 
-
+DOLLAR_OFF_PER_POUND = re.compile(r'\$([0-9]+)\sOFF\sPER\sPOUND')
+CENT_OFF_PER_POUND = re.compile(r'([0-9]+)¢\sOFF\sPER\sPOUND')
