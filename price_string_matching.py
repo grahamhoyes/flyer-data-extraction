ed29477d83244ae61dacd2af73d4ab0534d4e040
Graham Hoyes <accounts@grahamhoyes.com>
2020-01-25 23:21:09 -0500

    Most of price regex matching

--- price_string_matching.py
+++ price_string_matching.py
@@ -1,53 +1,126 @@
-
 import re
 import pandas as pd
+from ad_block import AdBlock
 
 
 def get_units():
     df_units = pd.read_csv("files/units_dictionary.csv", header=0)
-    units = df_units['units'].values
+    units = df_units["units"].values
     return units
 
 
 # Prices
-DOLLAR_PRICE_PER_UNIT = re.compile(r'\$([0-9]+)/(' + '|'.join(get_units()) + r')')  # $99/lb
-CENT_PRICE_PER_UNIT = re.compile(r'([0-9]+)¢/(' + '|'.join(get_units()) + r')')
+DOLLAR_PRICE_PER_UNIT = re.compile(
+    r"\$([0-9]+)/(" + "|".join(get_units()) + r")"
+)  # $199/lb
+CENT_PRICE_PER_UNIT = re.compile(r"([0-9]+)¢/(" + "|".join(get_units()) + r")")
 
-UNITS_PER_DOLLAR_PRICE = re.compile(r'([0-9]+)/\$([0-9]+)')  # 3/$1
-UNITS_PER_CENT_PRICE = re.compile(r'([0-9]+)/([0-9]+)¢')
+UNITS_PER_DOLLAR_PRICE = re.compile(r"([0-9]+)/\$([0-9]+)")  # 3/$1
+UNITS_PER_CENT_PRICE = re.compile(r"([0-9]+)/([0-9]+)¢")
 
-DOLLAR_PRICE = re.compile(r'\$([0-9]+)')  # $1
-CENT_PRICE = re.compile(r'([0-9]+)¢')
+DOLLAR_PRICE = re.compile(r"\$([0-9]+)")  # $1
+CENT_PRICE = re.compile(r"([0-9]+)¢")
 
 
 # TODO: All of these per unit
 # Savings in red
-BUY_ONE_GET_ONE_FREE = re.compile(r'FREE')
-HALF_OFF = re.compile(r'HALF\sOFF')
-PERCENTAGE_OFF = re.compile(r'([0-9]+)%\sOFF')
+BUY_ONE_GET_ONE_FREE = re.compile(r"BUY\sONE,*\sGET\sONE\sFREE")
+HALF_OFF = re.compile(r"HALF\sOFF")
+PERCENTAGE_OFF = re.compile(r"([0-9]+)%\sOFF")
+
+DOLLAR_OFF_PER_POUND = re.compile(r"\$([0-9]+)\sOFF\sPER\sPOUND")
+CENT_OFF_PER_POUND = re.compile(r"([0-9]+)¢\sOFF\sPER\sPOUND")
+
+DOLLAR_OFF = re.compile(r"\$([0-9]+)\sOFF")
+CENT_OFF = re.compile(r"([0-9]+)¢\sOFF")
 
 
 # Savings
-DOLLAR_OFF_PER_POUND = re.compile(r'\$([0-9]+)\sOFF\sPER\sPOUND')
-CENT_OFF_PER_POUND = re.compile(r'([0-9]+)¢\sOFF\sPER\sPOUND')
+SAVE_CENTS_ON_PER_UNIT = re.compile(r"SAVE.{0,7}([0-9]+)¢ on ([0-9]+)/(" + "|".join(get_units()) + r")")  # SAVE (up to) 98¢ on 2/lb
+SAVE_DOLLARS_ON_PER_UNIT = re.compile(r"SAVE.{0,7}\$([0-9]+) on ([0-9]+)/(" + "|".join(get_units()) + r")")
 
-SAVE_CENTS_ON = re.compile(r'SAVE ([0-9]+)¢ on ([0-9]+)')  # SAVE 98¢ on 2
-SAVE_DOLLARS_ON = re.compile(r'SAVE \$([0-9]+) on ([0-9]+)')
+DOLLAR_SAVE_PER_UNIT = re.compile(r"SAVE.{0,7}\$([0-9]+)/(" + "|".join(get_units()) + r")")
+CENT_SAVE_PER_UNIT = re.compile(r"SAVE.{0,7}([0-9]+)¢/(" + "|".join(get_units()) + r")")
 
-SAVE_CENTS_UP_TO_ON = re.compile(r'SAVE up to ([0-9]+)¢ on ([0-9]+)')  # SAVE up to 98¢ on 2
-SAVE_DOLLARS_UP_TO_ON = re.compile(r'SAVE up to \$([0-9]+)¢ on ([0-9]+)')
+SAVE_CENTS_ON = re.compile(r"SAVE.{0,7}([0-9]+)¢ on ([0-9]+)")  # SAVE (up to) 98¢ on 2
+SAVE_DOLLARS_ON = re.compile(r"SAVE.{0,7}\$([0-9]+) on ([0-9]+)")
 
-SAVE_CENTS_UP_TO = re.compile(r'SAVE up to ([0-9]+)¢')  # SAVE up to 98¢
-SAVE_DOLLARS_UP_TO = re.compile(r'SAVE up to \$([0-9]+)')
-
-DOLLAR_SAVE = re.compile(r'SAVE.{0,7}\$([0-9]+)')
-CENT_SAVE = re.compile(r'SAVE.{0,7}([0-9]+)¢')
-
-DOLLAR_OFF = re.compile(r'\$([0-9]+)\sOFF')
-CENT_OFF = re.compile(r'([0-9]+)¢\sOFF')
+DOLLAR_SAVE = re.compile(r"SAVE.{0,7}\$([0-9]+)")
+CENT_SAVE = re.compile(r"SAVE.{0,7}([0-9]+)¢")
 
 
-def match_price_in_block(text, ad):
-    match = DOLLAR_PRICE_PER_UNIT.match(text, re.IGNORECASE)
-    if match:
-        ad.uom = match.group(0)
+def match_price_in_block(text, ad: AdBlock):
+    """
+    Yes, there is a more efficient way to write this. I lack the time to figure it out.
+    :param text:
+    :param ad:
+    :return:
+    """
+    # Prices
+    dppu_match = DOLLAR_PRICE_PER_UNIT.match(text, re.IGNORECASE)
+    cppu_match = CENT_PRICE_PER_UNIT.match(text, re.IGNORECASE)
+    updp_match = UNITS_PER_DOLLAR_PRICE.match(text, re.IGNORECASE)
+    upcp_match = UNITS_PER_CENT_PRICE.match(text, re.IGNORECASE)
+    dp_match = DOLLAR_PRICE.match(text, re.IGNORECASE)
+    cp_match = CENT_PRICE.match(text, re.IGNORECASE)
+    bogo_match = BUY_ONE_GET_ONE_FREE.match(text, re.IGNORECASE)
+    ho_match = HALF_OFF.match(text, re.IGNORECASE)
+    po_match = PERCENTAGE_OFF.match(text, re.IGNORECASE)
+    dopp_match = DOLLAR_OFF_PER_POUND.match(text, re.IGNORECASE)
+    copp_match = CENT_OFF_PER_POUND.match(text, re.IGNORECASE)
+    do_match = DOLLAR_OFF.match(text, re.IGNORECASE)
+    co_match = CENT_OFF.match(text, re.IGNORECASE)
+
+    if dppu_match:
+        ad.set_dollar_price_per_unit(dppu_match.group(1), dppu_match.group(2), False)
+    elif cppu_match:
+        ad.set_dollar_price_per_unit(cppu_match.group(1), dppu_match.group(2), True)
+    elif updp_match:
+        ad.set_units_per_price(updp_match.group(1), updp_match.group(2), False)
+    elif upcp_match:
+        ad.set_units_per_price(upcp_match.group(1), updp_match.group(2), True)
+    elif bogo_match:
+        ad.set_buy_get_one_free()
+    elif ho_match:
+        ad.set_half_off()
+    elif po_match:
+        ad.set_percentage_off(po_match.group(1))
+    elif dopp_match:
+        ad.set_dollar_off_per_pound(dopp_match.group(1), False)
+    elif copp_match:
+        ad.set_dollar_off_per_pound(copp_match.group(1), True)
+    elif do_match:
+        ad.set_dollar_off(do_match.group(1), False)
+    elif co_match:
+        ad.set_dollar_off(co_match.group(1), True)
+    elif dp_match:
+        ad.set_dollar_price(dp_match.group(1), False)
+    elif cp_match:
+        ad.set_dollar_price(cp_match.group(1), True)
+
+    # Savings
+    sdopu_match = SAVE_DOLLARS_ON_PER_UNIT.match(text, re.IGNORECASE)
+    scopu_match = SAVE_CENTS_ON_PER_UNIT.match(text, re.IGNORECASE)
+    dspu_match = DOLLAR_SAVE_PER_UNIT.match(text, re.IGNORECASE)
+    cspu_match = CENT_SAVE_PER_UNIT.match(text, re.IGNORECASE)
+    sdo_match = SAVE_DOLLARS_ON.match(text, re.IGNORECASE)
+    sco_match = SAVE_CENTS_ON.match(text, re.IGNORECASE)
+    ds_match = DOLLAR_SAVE.match(text, re.IGNORECASE)
+    cs_match = CENT_SAVE.match(text, re.IGNORECASE)
+
+    if sdopu_match:
+        pass
+    elif scopu_match:
+        pass
+    elif dspu_match:
+        pass
+    elif cspu_match:
+        pass
+    elif sdo_match:
+        pass
+    elif sco_match:
+        pass
+    elif ds_match:
+        pass
+    elif cs_match:
+        pass
